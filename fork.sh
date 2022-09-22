cbd10ed8d67be4c86dad15a60f1fc623b0ec4fca
Eugene Andruszczenko <andruseu@amazon.com>
2022-09-22 14:13:01 -0400

    $(date)

--- fork.sh
+++ fork.sh
@@ -16,47 +16,15 @@ clear
 echo -e "${RESPONSE}";
 
 
-BEES=( Abel Abigail Adon Akira Akuma A-K-I Alex Balrog Birdie Blanka C-Viper Cammy Chun-Li Cody Dan Decapre Dee Jay Dhalsim Dudley E-Honda Eagle Ed El Fuerte Elena Eleven Evil Ryu F-A-N-G- Falke Fei Long G Geki Gen Gill Gouken Guile Guy Hakan Hugo Ibuki Ingrid Jamie Joe JP Juli Juni Juri Kage Karin Ken Kimberly Kolin Laura Lee Lily Lucia Luke M-Bison Maki Makoto Marisa Menat Mike Manon Nash Necalli Necro Oni Oro Poison Q R-Mika Rashid Remy Retsu Rolento Rose Rufus Ryu Sagat Sakura Sean Seth Shin-Akuma Sodom T-Hawk Twelve Urien Vega Violent Ken Yang Yun Zangief Zeku)
+STUDENTS=( Abel Abigail Adon Akira Akuma A-K-I Alex Balrog Birdie Blanka C-Viper Cammy Chun-Li Cody Dan Decapre Dee Jay Dhalsim Dudley E-Honda Eagle Ed El Fuerte Elena Eleven Evil-Ryu F-A-N-G- Falke Fei Long G Geki Gen Gill Gouken Guile Guy Hakan Hugo Ibuki Ingrid Jamie Joe JP Juli Juni Juri Kage Karin Ken Kimberly Kolin Laura Lee Lily Lucia Luke M-Bison Maki Makoto Marisa Menat Mike Manon Nash Necalli Necro Oni Oro Poison Q R-Mika Rashid Remy Retsu Rolento Rose Rufus Ryu Sagat Sakura Sean Seth Shin-Akuma Sodom T-Hawk Twelve Urien Vega Violent-Ken Yang Yun Zangief Zeku)
 cd .github/workflows
 
-for BEE in "${BEES[@]}";
+for STUDENT in "${STUDENTS[@]}";
 do
-  touch ${BEE}.yaml
-  tee ${BEE}.yaml << END
-Name: ${BEE}
-SchemaVersion: "1.0"
-RunMode: PARALLEL
-
- Optional - Set automatic triggers. Doing another no-op update
-Triggers:
-  - Type: Push
-    Branches:
-      - ${BEE}
-  - Type: PullRequest
-    Events:
-    - open
-    - revision
-    Branches:
-      - main
-Actions:
-  Friends:
-    Actions:
-      A:
-        Identifier: aws/build-gamma@v1
-        Inputs:
-          Sources:
-            - WorkflowSource
-        Configuration:
-          Steps:
-            - Run: echo "${BEE}!"
-      B:
-        Identifier: aws/build-gamma@v1
-        Inputs:
-          Sources:
-            - WorkflowSource
-        Configuration:
-          Steps:
-            - Run: echo "${BEE}!"
-END
+  git checkout ${STUDENT}
+  git merge main
+  git add .
+  git commit -am '$(DATE)'
+  git push
 done
-
+git checkout main
